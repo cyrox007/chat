22413c6e872e98cd4546bb566b4829e8406fae00
Alexandr T <66170352+cyrox007@users.noreply.github.com>
2026-09-15 17:41:11 +0300

    Fix Alembic online migration indentation

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -71,7 +71,7 @@ def run_migrations_online() -> None:
         context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
-        context.run_migrations()
+            context.run_migrations()
 
 
 if context.is_offline_mode():
